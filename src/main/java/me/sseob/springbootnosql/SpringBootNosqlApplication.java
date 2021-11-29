67c58a6fba8570922b7a529892bde9f7e1ac8e1b
hyun-sseob <orolsyeo@gmail.com>
2021-11-29 19:07:55 +0900

    initail commit 🕶

---
+++ src/main/java/me/sseob/springbootnosql/SpringBootNosqlApplication.java
@@ -0,0 +1,13 @@
+package me.sseob.springbootnosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNosqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sseob/springbootnosql/SpringBootNosqlApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sseob.springbootnosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootNosqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
