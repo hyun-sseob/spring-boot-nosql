bd012d88ec06fcca76bad2d8e81ebbe987fb0ad1
hyun-sseob <orolsyeo@gmail.com>
2021-11-29 20:24:06 +0900

    mongo data insert 🕶

--- src/main/java/me/sseob/springbootnosql/mongo/AccountRepository.java
+++ src/main/java/me/sseob/springbootnosql/mongo/AccountRepository.java
@@ -2,6 +2,8 @@ package me.sseob.springbootnosql.mongo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends MongoRepository<Account, String> {
-	
+	Optional<Account> findByUsername(String username);
 }
